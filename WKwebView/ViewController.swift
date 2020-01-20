37eb805ec0f9f25f95fd111cbd8ab19a3e4252eb
ikemoti <ikepon413@icloud.com>
2020-01-20 17:49:23 +0900

    Initial Commit

---
+++ WKwebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WKwebView
+//
+//  Created by USER on 2020/01/20.
+//  Copyright © 2020 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
