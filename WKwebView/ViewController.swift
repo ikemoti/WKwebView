3bb712c820e39c00f4a25797bb70fbf65bccf0c5
ikemoti <ikepon413@icloud.com>
2020-01-20 19:16:01 +0900

    できた

--- WKwebView/ViewController.swift
+++ WKwebView/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,WKNavigationDelegate {
 
+//初期化
+    var webview = WKWebView()
+    @IBOutlet weak var toolbar: UIToolbar!
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
+      //大きさ
+        webview.frame = CGRect(
+            x: 0,
+            y:toolbar.frame.size.height,
+            width: view.frame.size.width,
+            height:view.frame.size.height -
+                toolbar.frame.size.height*2)
+       //view追加
+        view.addSubview(webview)
+        
+        
+        webview.navigationDelegate = self
+    //sstring型をURL型にキャスト
+        let url = URL(string:"https://www.google.com/webhp?hl=ja&sa=X&ved=0ahUKEwiGw6TQ85HnAhUuyYsBHdQtAQsQPAgH")
+        
+        let request  = URLRequest(url: url!)
+        
+        webview.load(request)
+        }
+    //ロードが開始された時
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        print("ロード完了")
+        
+        indicator.stopAnimating()
     }
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func go(_ sender: Any) {
+        webview.goForward()
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        webview.goBack()
+    }
 }
 
